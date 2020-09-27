30b2c061f0c439547db3cd299bcd539d688bdbd4
Lapsa <agusdinar@hotmail.com>
2020-09-27 17:21:25 -0300

    fix app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express()
 
 const port=process.env.PORT||3000
 
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 app.get("/",(req,res)=>{
     res.render("public/index")
